724df6e1c058882cd72cb5e09a1795f08b1a944d
f1she3 <f1she3@protonmail.com>
2023-02-18 00:32:43 +0100

    Finished news

--- fetch-fortnite-news/lambda_function.py
+++ fetch-fortnite-news/lambda_function.py
@@ -1,6 +1,12 @@
 import json
 import logging
 import asyncio
+from telegram import Bot
+from handlers import (
+    constants,
+    newsHandler
+)
+from telegram.constants import ParseMode
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -13,3 +19,13 @@ def lambda_handler(event, context):
 
 
 async def main(event, context):
+    bot = Bot(constants.TELEGRAM_TOKEN)
+    latest_news = await newsHandler.getLatestNews()
+    async with bot:
+        for content in latest_news:
+            await bot.send_message(chat_id=constants.GROUP_CHAT_ID, text=content, parse_mode=ParseMode.HTML)
+            continue
+"""
+if __name__ == '__main__':
+    lambda_handler("", "")
+"""
