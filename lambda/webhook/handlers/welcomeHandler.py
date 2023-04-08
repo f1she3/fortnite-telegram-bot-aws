e427a84b7af1305829ef1fc67e34178d67365a70
f1she3 <f1she3@protonmail.com>
2023-04-08 15:51:27 +0200

    Now sends private message to welcome new users

--- lambda/webhook/handlers/welcomeHandler.py
+++ lambda/webhook/handlers/welcomeHandler.py
@@ -8,8 +8,12 @@ async def welcome(update: Update, context: ContextTypes.DEFAULT_TYPE):
     new_members = update.message.new_chat_members
     for member in new_members:
         msg = helpHandler.get_help_msg_welcome(member)
+        if member.is_bot:
+            chatId = update.effective_chat.id
+        else:
+            chatId = member.id
         await context.bot.send_message(
-            chat_id=update.effective_chat.id,
+            chat_id=chatId,
             text=msg,
             parse_mode=ParseMode.HTML
         )
